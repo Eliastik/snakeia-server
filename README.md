c20cce48bf2feb53d4353911ebe82cd79d8aefcf
Eliastik <26941242+Eliastik@users.noreply.github.com>
2020-02-01 19:28:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,93 @@
+<img src="https://raw.githubusercontent.com/Eliastik/snakeia-server/master/logo.png" width="300" alt="SnakeIA Server" />
+
+# English
+
+A server for my [SnakeIA](https://github.com/Eliastik/snakeia) game, written in JavaScript with the Socket.IO library and running on Node.js.
+
+* Github repository: [https://github.com/Eliastik/snakeia-server](https://github.com/Eliastik/snakeia-server)
+
+## About this server
+
+* Still in development
+* Made in France by Eliastik - [eliastiksofts.com](http://eliastiksofts.com) - Contact : [eliastiksofts.com/contact](http://eliastiksofts.com/contact)
+* License: GNU GPLv3 (see LICENCE.txt file)
+
+## How to run the server
+
+You can run the server on your machine. To do this, you have to install Node.js and npm.
+
+To install Node.js and npm for your OS, read this page: https://docs.npmjs.com/getting-started/installing-node
+
+Git clone the repository and cd to the project directory (or download it directly from Github):
+````
+git clone https://github.com/Eliastik/snakeia-server.git
+cd snakeia-server
+````
+
+Install the dependencies:
+````
+npm install
+````
+
+Then to run the server:
+````
+npm run server
+````
+
+The server will by default run on the port 3000.
+
+# Français
+
+Un serveur pour mon jeu [SnakeIA](https://github.com/Eliastik/snakeia), écrit en JavaScript avec la bibliothèque logicielle Socket.IO et tournant via Node.js.
+
+* Dépôt Github : [https://github.com/Eliastik/snakeia-server](https://github.com/Eliastik/snakeia-server)
+
+## À propos de ce serveur
+
+* Encore en développement
+* Made in France by Eliastik - [eliastiksofts.com](http://eliastiksofts.com) - Contact : [eliastiksofts.com/contact](http://eliastiksofts.com/contact)
+* Licence : GNU GPLv3 (voir le fichier LICENCE.txt)
+
+## Comment lancer le serveur
+
+Vous pouvez lancer le serveur sur votre machine. Pour cela, vous devez avoir installé Node.js et npm.
+
+Pour installer Node.js et npm sur votre système, suivez le guide : https://docs.npmjs.com/getting-started/installing-node
+
+Effectuez un clonage du dépôt et déplacez-vous dedans (ou téléchargez-le directement depuis Github) :
+````
+git clone https://github.com/Eliastik/snakeia-server.git
+cd snakeia-server
+````
+
+Puis lancez cette commande pour installer les dépendances :
+````
+npm install
+````
+
+Puis pour lancer le serveur :
+````
+npm run server
+````
+
+Le serveur va se lancer sur le port 3000 par défaut.
+
+## Déclaration de licence
+
+Copyright (C) 2020 Eliastik (eliastiksofts.com)
+
+Ce programme est un logiciel libre ; vous pouvez le redistribuer ou le modifier suivant les termes de la GNU General Public License telle que publiée par la Free Software Foundation ; soit la version 3 de la licence, soit (à votre gré) toute version ultérieure.
+
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie tacite de QUALITÉ MARCHANDE ou d'ADÉQUATION à UN BUT PARTICULIER. Consultez la GNU General Public License pour plus de détails.
+
+Vous devez avoir reçu une copie de la GNU General Public License en même temps que ce programme ; si ce n'est pas le cas, consultez http://www.gnu.org/licenses.
+
+----
+
+Copyright (C) 2020 Eliastik (eliastiksofts.com)
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
---
+++ index.js
@@ -0,0 +1,227 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var snakeia = require("snakeia");
+
+var Snake = snakeia.Snake;
+var Grid = snakeia.Grid;
+var GameEngine = snakeia.GameEngine;
+var GameConstants = snakeia.GameConstants;
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  var grid = new Grid();
+  grid.init();
+  var snake = new Snake();
+  var game = new GameEngine(grid, snake);
+
+  socket.emit("init", {
+    "snakes": JSON.parse(JSON.stringify(game.snakes)),
+    "grid": JSON.parse(JSON.stringify(game.grid)),
+    "enablePause": game.enablePause,
+    "enableRetry": game.enableRetry,
+    "progressiveSpeed": game.progressiveSpeed,
+    "offsetFrame": game.speed * GameConstants.Setting.TIME_MULTIPLIER,
+    "errorOccurred": game.errorOccurred
+  });
+
+  game.onReset(function() {
+    socket.emit("reset", {
+      "paused": game.paused,
+      "isReseted": game.isReseted,
+      "exited": game.exited,
+      "grid": JSON.parse(JSON.stringify(game.grid)),
+      "numFruit": game.numFruit,
+      "ticks": game.ticks,
+      "scoreMax": game.scoreMax,
+      "gameOver": game.gameOver,
+      "gameFinished": game.gameFinished,
+      "gameMazeWin": game.gameMazeWin,
+      "starting": game.starting,
+      "initialSpeed": game.initialSpeed,
+      "speed": game.speed,
+      "snakes": JSON.parse(JSON.stringify(game.snakes)),
+      "offsetFrame": game.speed * GameConstants.Setting.TIME_MULTIPLIER,
+      "confirmReset": false,
+      "confirmExit": false,
+      "getInfos": false,
+      "getInfosGame": false,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+
+  game.onStart(function() {
+    socket.emit("start", {
+      "snakes": JSON.parse(JSON.stringify(game.snakes)),
+      "grid": JSON.parse(JSON.stringify(game.grid)),
+      "starting": game.starting,
+      "countBeforePlay": game.countBeforePlay,
+      "paused": game.paused,
+      "isReseted": game.isReseted,
+      "confirmReset": false,
+      "confirmExit": false,
+      "getInfos": false,
+      "getInfosGame": false,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+
+  game.onPause(function() {
+    socket.emit("pause", {
+      "paused": game.paused,
+      "confirmReset": false,
+      "confirmExit": false,
+      "getInfos": false,
+      "getInfosGame": false,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+
+  game.onContinue(function() {
+    socket.emit("continue", {
+      "confirmReset": false,
+      "confirmExit": false,
+      "getInfos": false,
+      "getInfosGame": false,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+
+  game.onStop(function() {
+    socket.emit("stop", {
+      "paused": game.paused,
+      "scoreMax": game.scoreMax,
+      "gameOver": game.gameOver,
+      "gameFinished": game.gameFinished,
+      "confirmReset": false,
+      "confirmExit": false,
+      "getInfos": false,
+      "getInfosGame": false,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+
+  game.onExit(function() {
+    socket.emit("exit", {
+      "paused": game.paused,
+      "gameOver": game.gameOver,
+      "gameFinished": game.gameFinished,
+      "exited": game.exited,
+      "confirmReset": false,
+      "confirmExit": false,
+      "getInfos": false,
+      "getInfosGame": false,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+
+  game.onKill(function() {
+    socket.emit("kill", {
+      "paused": game.paused,
+      "gameOver": game.gameOver,
+      "killed": game.killed,
+      "snakes": JSON.parse(JSON.stringify(game.snakes)),
+      "gameFinished": game.gameFinished,
+      "grid": JSON.parse(JSON.stringify(game.grid)),
+      "confirmReset": false,
+      "confirmExit": false,
+      "getInfos": false,
+      "getInfosGame": false,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+
+  game.onScoreIncreased(function() {
+    socket.emit("scoreIncreased", {
+      "snakes": JSON.parse(JSON.stringify(game.snakes)),
+      "grid": JSON.parse(JSON.stringify(game.grid)),
+      "scoreMax": game.scoreMax,
+      "gameFinished": game.gameFinished,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+  
+  game.onUpdate(function() {
+    socket.emit("update", {
+      "paused": game.paused,
+      "isReseted": game.isReseted,
+      "exited": game.exited,
+      "grid": JSON.parse(JSON.stringify(game.grid)),
+      "numFruit": game.numFruit,
+      "ticks": game.ticks,
+      "scoreMax": game.scoreMax,
+      "gameOver": game.gameOver,
+      "gameFinished": game.gameFinished,
+      "gameMazeWin": game.gameMazeWin,
+      "starting": game.starting,
+      "initialSpeed": game.initialSpeed,
+      "speed": game.speed,
+      "snakes": JSON.parse(JSON.stringify(game.snakes)),
+      "countBeforePlay": game.countBeforePlay,
+      "numFruit": game.numFruit,
+      "offsetFrame": 0,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+
+  game.onUpdateCounter(function() {
+    socket.emit("updateCounter", {
+      "paused": game.paused,
+      "isReseted": game.isReseted,
+      "exited": game.exited,
+      "grid": JSON.parse(JSON.stringify(game.grid)),
+      "numFruit": game.numFruit,
+      "ticks": game.ticks,
+      "scoreMax": game.scoreMax,
+      "gameOver": game.gameOver,
+      "gameFinished": game.gameFinished,
+      "gameMazeWin": game.gameMazeWin,
+      "starting": game.starting,
+      "initialSpeed": game.initialSpeed,
+      "speed": game.speed,
+      "snakes": JSON.parse(JSON.stringify(game.snakes)),
+      "countBeforePlay": game.countBeforePlay,
+      "numFruit": game.numFruit,
+      "errorOccurred": game.errorOccurred
+    });
+  });
+
+  socket.on("reset", function() {
+    game.reset();
+  });
+
+  socket.on("start", function() {
+    game.start();
+  });
+
+  socket.on("finish", function() {
+    game.stop(true);
+  });
+
+  socket.on("stop", function() {
+    game.stop(false);
+  });
+
+  socket.on("pause", function() {
+    game.pause();
+  });
+
+  socket.on("kill", function() {
+    game.kill();
+  });
+
+  socket.on("exit", function() {
+    game.exit();
+  });
+
+  socket.on("key", function(key) {
+    game.lastKey = key;
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
